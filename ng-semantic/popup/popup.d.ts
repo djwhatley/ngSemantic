33e149dee7161c3efae23f369e6f8518a489c154
Arnaud Bosc <bosc.arno@gmail.com>
2016-09-08 23:17:34 +0200

    chore: improve file paths & npm build

--- ng-semantic/popup/popup.d.ts
+++
@@ -1,8 +0,0 @@
-import { ElementRef } from "@angular/core";
-export declare class SemanticPopupComponent {
-    popup: ElementRef;
-    private visible;
-    private element;
-    show(element: Event, data?: {}): void;
-    hide(): void;
-}
